0307d6f79792d7ce26cc76d3bd60a53fbdee75cf
devdavibarreto <davibarretojkda@gmail.com>
2026-01-03 08:48:25 -0300

    Javadoc , package and comment

---
+++ src/academy/devdojo/maratonajava/introducao/HelloDevDojo.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ *  This is comment JavaDoc
+ *  Clicking on something like println and pressing Ctrl + B
+ *lets you see what it does, and Ctrl + Q shows the documentation.
+ *
+ * Every public method in Your class must have a JavaDoc
+ * ctrl + alt+ q rendering
+ */
+
+public class HelloDevDojo {
+    public static void main(String[] args) {
+        // Single line comment (Evitar)
+        /*
+        Multiple line comment (Evitar)
+
+         */
+        System.out.println("GOD IS PERFECT");
+    }
+}
