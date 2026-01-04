c820985012218bac1eaa5cfb33935d28a3c06562
devdavibarreto <davibarretojkda@gmail.com>
2026-01-04 16:01:21 -0300

    Primitive types  class 2 Update in comment

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,13 +4,13 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // Primitive types store a simple value in memory
 // Always written in lowercase
-// There are 8: int, double, float, char, byte, short, long, boolean
+// There are 8 Primitive types: int, double, float, char, byte, short, long, boolean
 // Variables should always be in English
         // Sempre se preocupar com o tamanho da variavel e ver o melhor a ser usado entre os 8
 int age = 10;
 long numeroGrande = 100000;
-double salarioDouble = 2000;
-float salarioFloat = 2500;
+double salarioDouble = 2000; //Valores Decimais
+float salarioFloat = 2500; // Valores Decimais 
 byte idadeBye = 127;
 short idadeShort= 10;
 boolean verdadeiro = true;
