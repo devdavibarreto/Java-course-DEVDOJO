043c70147cff202d00a7c8223dfc8125cf8c24af
devdavibarreto <davibarretojkda@gmail.com>
2026-01-05 00:28:56 -0300

    Primitive types  class 3 Update in code class 2

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -8,10 +8,13 @@ public class Aula02TiposPrimitivos {
 // Variables should always be in English
         // Sempre se preocupar com o tamanho da variavel e ver o melhor a ser usado entre os 8
 int age = 10;
+int age1 = (int) 10000000000000000L; // Pq tem que tomar cuidado para não forçar  ele colocar um tipo dentro de um outro tipo
+        // Essa pratíca se chama CAST mesma coisa para double float etc  quando cabe vc tem o resultado quando
+        // não cabe vc vai ter um "corrompido".
 long numeroGrande = 100000;
-double salarioDouble = 2000; //Valores Decimais
-float salarioFloat = 2500; // Valores Decimais 
-byte idadeBye = 127;
+double salarioDouble = 20000; //Valores Decimais
+float salarioFloat = 2500; // Valores Decimais
+byte idadeBye = 127; // exceção da regra do ultimo comentario
 short idadeShort= 10;
 boolean verdadeiro = true;
 boolean falso = false;
@@ -19,5 +22,9 @@ char caractere = 87; // Ler no APDFS
 // Masculino e femenino
 
         System.out.println(age); //ctrl + d duplica a linha
+        System.out.println(age1); //ctrl + d duplica a linha
     }
 }
+// Toda vez que você compila escrevendo o número direto no código ele vai entender que aquilo e um INT vc tem que
+// Especificar !! exemplo coloca um L no int
+// Cast não é uma boa pratica a ser feita !!
