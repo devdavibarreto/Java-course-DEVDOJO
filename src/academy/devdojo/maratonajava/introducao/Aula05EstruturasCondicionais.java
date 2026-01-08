0496b597319f42f2fea496766555aa68f1794950
devdavibarreto <davibarretojkda@gmail.com>
2026-01-08 00:57:53 -0300

    Conditional structures, use of "IF" in specific cases, and examples from Java exams. Update in APDF added IF

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        // O if só é executado se a condicional que estiver dentro dele for verdadeira !!
+        /**
+         * Uma forma tambem muito vista do if
+         * boolean autorizado = idade >= 20;
+         * Usando o operador de negação !
+         *if (autorizado == false){
+         *             System.out.println(" Não esta autorizado a iniciar o processo de habilitação");
+         *         }
+         *
+         */
+        int idade = 20;
+        boolean isAutorizado = idade >= 18;
+        if (isAutorizado){
+            System.out.println("Autorizado a iniciar o processo de habilitação");
+        }
+
+        if (!isAutorizado){
+            System.out.println(" Não esta autorizado a iniciar o processo de habilitação");
+        }
+
+        System.out.println("Fora do if");
+
+    }
+}
