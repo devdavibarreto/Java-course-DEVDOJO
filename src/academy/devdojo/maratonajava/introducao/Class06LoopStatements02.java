a65e1e2fb9f4a5ba252da3308b662cc90de53bf5
devdavibarreto <davibarretojkda@gmail.com>
2026-01-15 19:02:41 -0300

    From this class on, everything will be in English

---
+++ src/academy/devdojo/maratonajava/introducao/Class06LoopStatements02.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Class06LoopStatements02 {
+    public static void main(String[] args) {
+        // From this class on, everything will be in English
+    }
+}
