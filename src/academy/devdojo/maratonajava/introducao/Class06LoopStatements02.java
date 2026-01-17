f62696bc354b0c1fb1d26401b51587771a8fbb32
devdavibarreto <davibarretojkda@gmail.com>
2026-01-16 22:36:59 -0300

    exercise

--- src/academy/devdojo/maratonajava/introducao/Class06LoopStatements02.java
+++ src/academy/devdojo/maratonajava/introducao/Class06LoopStatements02.java
@@ -4,5 +4,10 @@ public class Class06LoopStatements02 {
     public static void main(String[] args) {
         // From this class on, everything will be in English
         // https://www.youtube.com/watch?v=RbM_ZzEi8MY&list=PL0Un1HNdB4jGKw5szJrQETqJTlZKyKReu&index=29
+
+         double par = 998002;
+
+
+        }
     }
 }
