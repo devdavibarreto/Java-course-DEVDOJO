d857583a0967d0c8bff34334cbb3eaec299192c2
devdavibarreto <davibarretojkda@gmail.com>
2026-01-17 20:01:15 -0300

    Optimized exercise and solution from the teacher!! Loop , for

--- src/academy/devdojo/maratonajava/introducao/Class06LoopStatements02.java
+++ src/academy/devdojo/maratonajava/introducao/Class06LoopStatements02.java
@@ -5,9 +5,31 @@ public class Class06LoopStatements02 {
         // From this class on, everything will be in English
         // https://www.youtube.com/watch?v=RbM_ZzEi8MY&list=PL0Un1HNdB4jGKw5szJrQETqJTlZKyKReu&index=29
 
-         double par = 998002;
+        // Print all even numbers  from 0 to 100000
 
+        //the shortcut is for
 
+// TEACHER'S OPTIMIZED RESOLUTION
+
+        // I need to think about scalable programs, that is, code maintainability. What if I solve it this way and the condition changes?
+//        for (int i = 0; i <= 100000 ; i+=2) {
+//            System.out.println("i);
+//        }
+
+// Temos que tornar uma solução mais flexível
+
+        for (int i = 0; i < 100000; i++) {
+            if (i % 2 == 0) {
+                System.out.println("This Number is even " + i);
+            }
         }
+
+        // My resolution !!
+//        for (int i = 0; i <= 100000; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println("This Number is even " + i);
+//            }
+//        }
+
     }
 }
